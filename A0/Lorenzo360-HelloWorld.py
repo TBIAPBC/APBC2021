c9b1aa4712add07bc937d4e59577ee4c4e28d3b2
Lorenzo360 <lorenz.perschy@icloud.com>
2021-03-05 22:47:12 +0100

    A0 task completed - added missing lines

--- A0/Lorenzo360-HelloWorld.py
+++ A0/Lorenzo360-HelloWorld.py
@@ -1,1 +1,9 @@
-print("Hello World")
+#Proramm prints Hello World and the content of HelloWorld-test1.in
+print("Hello World!")
+try:
+    file = open("HelloWorld-test1.in", "r")
+    for line in file.readlines():
+        print(line, end='')
+    file.close()
+except Exception as e:
+    print(e)
