73b2c9873a1d99571885d692ee8bea2af84c9b48
Lorenzo360 <lorenz.perschy@icloud.com>
2021-03-06 08:43:24 +0100

    Adapted to specification: accepts a single file name

--- A0/Lorenzo360-HelloWorld.py
+++ A0/Lorenzo360-HelloWorld.py
@@ -1,9 +1,17 @@
-# Script prints Hello World! and the content of HelloWorld-test1.in
+# Script prints Hello World! and the content of a text file provided by command line
+import sys
+
 print("Hello World!")
+
+if len(sys.argv) != 2:
+    print("Please provide just one text file!")
+    print("Aborting")
+    sys.exit(1)
 try:
-    file = open("HelloWorld-test1.in", "r")
+    file = open(sys.argv[1], "r")
     for line in file.readlines():
         print(line, end='')
     file.close()
 except Exception as e:
     print(e)
+sys.exit(0)
