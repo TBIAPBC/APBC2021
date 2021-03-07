32bf44c03a913b6efee5cff6938665639a1de9af
MarkusBeicht <79964677+MarkusBeicht@users.noreply.github.com>
2021-03-07 20:44:06 +0100

    Update MarkusBeicht-HelloWorld.py

--- A0/MarkusBeicht-HelloWorld.py
+++ A0/MarkusBeicht-HelloWorld.py
@@ -7,16 +7,19 @@ import sys
 
 print("Hello World!")
 
+#Checks if more than one input file was used
 if len(sys.argv) > 2:
     print("This program only accepts a single file!")
     print("Shutting down!")
     sys.exit(1)
 
+#Checks if no input file was used
 if len(sys.argv) < 2:
     print("No file name was given!")
     print("Shutting down!")
     sys.exit(1)
 
+#Reads the input file and prints it
 try:
     file = open(sys.argv[1], "r")
     for line in file.readlines():
