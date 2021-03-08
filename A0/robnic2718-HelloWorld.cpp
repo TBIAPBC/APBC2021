23a13a1f414ad595e48c305b375db7ec6950c4f5
robnic2718 <nicolas.r.nemeth@gmail.com>
2021-03-09 00:44:03 +0100

    Solution A0

---
+++ A0/robnic2718-HelloWorld.cpp
@@ -0,0 +1,34 @@
+/*
+Please either compile the source yourself with a C++ compiler
+or
+use my exe-file "robnic2718-HelloWorld" and try this on the command line:
+> ./robnic2718-HelloWorld HelloWorld-test1.in
+*/
+
+#include<iostream>
+#include<fstream>
+#include<stdexcept>
+using namespace std;
+
+// argc = argument count on command-line including the program
+// args = array of char pointers
+int main(int argc, char **args) {
+
+  if(argc != 2)
+    throw runtime_error("\nPlease provide one input file.\n");
+
+  // initialize instance of stream class (constructor opens file)
+  ifstream input_file{*++args};
+  // declare string variable
+  string line;
+
+  cout << "Hello World!\n";
+
+  if(input_file.is_open())
+    while(getline(input_file, line))
+      cout << line << endl;
+
+  input_file.close();
+
+  return 0;
+}
