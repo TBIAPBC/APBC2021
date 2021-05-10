ab69c1b08df358d94cb51d450dcd117147b6e507
Lino-Boehler <lino.boehler@gmx.net>
2021-05-10 13:39:51 +0200

    I don't konw if it is working was not able to pip insatll it, neither was I with other packages on git hub

---
+++ Lino_Boehler_A5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+   name='Lino_Boehler_A5',
+   version='0.1.0',
+   author='Lino Boehler',
+   author_email='lino.boehler@gmx.net',
+   #packages=["A0","A1","A2","A3","A4"],
+   #scripts=['bin/script1','bin/script2'],
+   #url='http://pypi.python.org/pypi/PackageName/',
+   license="MIT-License",
+   description='First Package include all Exercises so far',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       "numpy"    
+       
+   ],
+ 		
+)
+
